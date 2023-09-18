9d27810560cc37ea3577a214a1c885dd7e867823
Harsh <hsharma@mun.ca>
2023-09-18 19:23:07 -0230

    Updates: 1. Updated the project section: 	- Removed the owl crousal 	- Added card gallery

--- src/PortfolioContainer/Testimonial/Carousel.js
+++ src/PortfolioContainer/Testimonial/Carousel.js
@@ -1,25 +1,16 @@
 import React from "react";
-import {
-	MDBCard,
-	MDBCardBody,
-	MDBCardTitle,
-	MDBCardText,
-	MDBCardImage,
-	MDBBtn,
-} from "mdb-react-ui-kit";
-import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
-import ScrollService from "../../utilities/ScrollService";
-import Animations from "../../utilities/Animations";
-import "./Carousel.min.css";
-import OwlCarousel from "react-owl-carousel2";
 import "react-owl-carousel2/lib/styles.css";
 import "react-owl-carousel2/src/owl.theme.default.css";
+import Animations from "../../utilities/Animations";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import "./Carousel.min.css";
+import oneStopAgro from "./images/OneStopAgro.svg";
+import co2by from "./images/co2by.png";
+import gameOfFocus from "./images/game_of_focus.svg";
+import mentNL from "./images/ment-nl.webp";
 import portfolioImage from "./images/portfolioImage.svg";
 import sbTelecomWebstore from "./images/sb_telecom_webstore.webp";
-import gameOfFocus from "./images/game_of_focus.svg";
-import oneStopAgro from "./images/OneStopAgro.svg";
-import mentNL from "./images/ment-nl.webp";
-import co2by from "./images/co2by.png";
 
 export default function Testimonial(props) {
 	let fadeInScreenHandler = (screen) => {
@@ -29,24 +20,7 @@ export default function Testimonial(props) {
 	};
 
 	ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-	const options = {
-		items: 1,
-		nav: true,
-		rewind: true,
-		autoplay: true,
-		margin: 50,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			450: {
-				items: 1,
-			},
-			600: {
-				items: 1,
-			},
-		},
-	};
+
 	return (
 		<div>
 			<ScreenHeading
@@ -54,291 +28,215 @@ export default function Testimonial(props) {
 				subHeading={"Have a look at my projects"}
 			/>
 			<section className="testimonial-section fade-in" id={props.id || ""}>
-				<OwlCarousel options={options}>
-					<div className="item">
-						<div className="card">
-							<MDBCard
+				<div className="row">
+					<div className="col-lg-4 col-md-6 mb-4">
+						<div className="card" style={{ width: "22rem", height: "32rem" }}>
+							<img
 								style={{
 									width: "22rem",
-									height: "32rem",
+									height: "12rem",
+									objectFit: "cover",
 								}}
-							>
-								<MDBCardImage
-									style={{
-										width: "22rem",
-										height: "12rem",
-										backgroundSize: "cover",
-									}}
-									src={co2by}
-									position="top"
-									alt="..."
-								/>
-								<MDBCardBody
-									style={{
-										textAlign: "center",
-									}}
+								src={co2by}
+								alt="CO2BY"
+							/>
+							<div className="card-body" style={{ textAlign: "center" }}>
+								<h5
+									className="card-title"
+									style={{ fontWeight: "bold", fontSize: "25px" }}
 								>
-									<MDBCardTitle
-										style={{
-											fontWeight: "bold",
-											fontSize: "25px",
-											textAlign: "center",
-										}}
-									>
-										{" "}
-										CO2BY{" "}
-									</MDBCardTitle>{" "}
-									<MDBCardText>
-										CO2By is a project developed by a team of five students with
-										diverse backgrounds during the Hackfrost 3.0 hackathon. {" "}
-									</MDBCardText>{" "}
-									<MDBBtn
-										style={{
-											background: "#ff5823",
-										}}
-										href="https://devpost.com/software/co2by"
-									>
-										{" "}
-										View{" "}
-									</MDBBtn>{" "}
-								</MDBCardBody>{" "}
-							</MDBCard>{" "}
-						</div>{" "}
+									CO2BY
+								</h5>
+								<p className="card-text">
+									CO2By is a project developed by a team of five students with
+									diverse backgrounds during the Hackfrost 3.0 hackathon.
+								</p>
+								<a
+									href="https://devpost.com/software/co2by"
+									className="btn btn-primary"
+									style={{ background: "#ff5823" }}
+								>
+									View
+								</a>
+							</div>
+						</div>
 					</div>
-					<div className="item">
-						<div className="card">
-							<MDBCard
+
+					<div className="col-lg-4 col-md-6 mb-4">
+						<div className="card" style={{ width: "22rem", height: "32rem" }}>
+							<img
 								style={{
 									width: "22rem",
-									height: "32rem",
+									height: "12rem",
+									objectFit: "cover",
 								}}
-							>
-								<MDBCardImage
-									style={{
-										width: "22rem",
-										height: "12rem",
-										backgroundSize: "cover",
-									}}
-									src={portfolioImage}
-									position="top"
-									alt="..."
-								/>
-								<MDBCardBody
-									style={{
-										textAlign: "center",
-									}}
+								src={portfolioImage}
+								alt="Portfolio Website"
+							/>
+							<div className="card-body" style={{ textAlign: "center" }}>
+								<h5
+									className="card-title"
+									style={{ fontWeight: "bold", fontSize: "25px" }}
 								>
-									<MDBCardTitle
-										style={{
-											fontWeight: "bold",
-											fontSize: "25px",
-											textAlign: "center",
-										}}
-									>
-										{" "}
-										Portfolio Website{" "}
-									</MDBCardTitle>{" "}
-									<MDBCardText>
-										A Personal Portfolio website to showcase all my details and
-										projects at one place using React JS, Bootstrap.{" "}
-									</MDBCardText>{" "}
-									<MDBBtn
-										style={{
-											background: "#ff5823",
-										}}
-										href="#"
-									>
-										{" "}
-										View{" "}
-									</MDBBtn>{" "}
-								</MDBCardBody>{" "}
-							</MDBCard>{" "}
-						</div>{" "}
+									Portfolio Website
+								</h5>
+								<p className="card-text">
+									A Personal Portfolio website to showcase all my details and
+									projects at one place using React JS, Bootstrap.
+								</p>
+								<a
+									href="#"
+									className="btn btn-primary"
+									style={{ background: "#ff5823" }}
+								>
+									View
+								</a>
+							</div>
+						</div>
 					</div>
-					<div className="item">
-						<div className="card">
-							<MDBCard
+
+					<div className="col-lg-4 col-md-6 mb-4">
+						<div className="card" style={{ width: "22rem", height: "32rem" }}>
+							<img
 								style={{
 									width: "22rem",
-									height: "32rem",
+									height: "12rem",
+									objectFit: "cover",
 								}}
-							>
-								<MDBCardImage src={oneStopAgro} position="top" alt="..." />
-								<MDBCardBody
-									style={{
-										textAlign: "center",
-									}}
+								src={oneStopAgro}
+								alt="OneStopAgro"
+							/>
+							<div className="card-body" style={{ textAlign: "center" }}>
+								<h5
+									className="card-title"
+									style={{ fontWeight: "bold", fontSize: "25px" }}
 								>
-									<MDBCardTitle
-										style={{
-											fontWeight: "bold",
-											fontSize: "25px",
-											textAlign: "center",
-										}}
-									>
-										{" "}
-										OneStopAgro{" "}
-									</MDBCardTitle>{" "}
-									<MDBCardText>
-										Project build for Hack Frost NL to provide complete solution
-										from early disease detection in crop to marketplace to sell
-										the product using Python, Tensorflow, Keras, Scikit - learn,
-										JS, HTML, CSS, Bootstrap{" "}
-									</MDBCardText>{" "}
-									<MDBBtn
-										style={{
-											background: "#ff5823",
-										}}
-										href="https://devpost.com/software/one-stop-agro?ref_content=user-portfolio&ref_feature=in_progress%5C"
-									>
-										{" "}
-										View{" "}
-									</MDBBtn>{" "}
-								</MDBCardBody>{" "}
-							</MDBCard>{" "}
-						</div>{" "}
+									OneStopAgro
+								</h5>
+								<p className="card-text">
+									Project built for Hack Frost NL to provide a complete solution
+									from early disease detection in crops to marketplace to sell
+									the product using Python, Tensorflow, Keras, Scikit-learn, JS,
+									HTML, CSS, Bootstrap.
+								</p>
+								<a
+									href="https://devpost.com/software/one-stop-agro?ref_content=user-portfolio&ref_feature=in_progress%5C"
+									className="btn btn-primary"
+									style={{ background: "#ff5823" }}
+								>
+									View
+								</a>
+							</div>
+						</div>
 					</div>
-					<div className="item">
-						<div className="card">
-							<MDBCard
+
+					<div className="col-lg-4 col-md-6 mb-4">
+						<div className="card" style={{ width: "22rem", height: "32rem" }}>
+							<img
 								style={{
 									width: "22rem",
-									height: "32rem",
+									height: "12rem",
+									objectFit: "cover",
 								}}
-							>
-								<MDBCardImage
-									style={{
-										width: "22rem",
-										height: "12rem",
-										backgroundSize: "cover",
-									}}
-									src={mentNL}
-									position="top"
-									alt="..."
-								/>
-								<MDBCardBody
-									style={{
-										textAlign: "center",
-									}}
+								src={mentNL}
+								alt="Ment-NL Website"
+							/>
+							<div className="card-body" style={{ textAlign: "center" }}>
+								<h5
+									className="card-title"
+									style={{ fontWeight: "bold", fontSize: "25px" }}
 								>
-									<MDBCardTitle
-										style={{
-											fontWeight: "bold",
-											fontSize: "25px",
-											textAlign: "center",
-										}}
-									>
-										{" "}
-										Ment-NL Website{" "}
-									</MDBCardTitle>{" "}
-									<MDBCardText>
-										TechNL FWD Hackathon project to help mitigate the problem of
-										tech-sector employment shortage by providing mentorship and
-										open source project contribution platform for new developers{" "}
-									</MDBCardText>{" "}
-									<MDBBtn
-										style={{
-											background: "#ff5823",
-										}}
-										href="https://king-knight-harsh.github.io/ment-nl/"
-									>
-										{" "}
-										View{" "}
-									</MDBBtn>{" "}
-								</MDBCardBody>{" "}
-							</MDBCard>{" "}
-						</div>{" "}
+									Ment-NL Website
+								</h5>
+								<p className="card-text">
+									TechNL FWD Hackathon project to help mitigate the problem of
+									tech-sector employment shortage by providing mentorship and
+									open source project contribution platform for new developers.
+								</p>
+								<a
+									href="https://king-knight-harsh.github.io/ment-nl/"
+									className="btn btn-primary"
+									style={{ background: "#ff5823" }}
+								>
+									View
+								</a>
+							</div>
+						</div>
 					</div>
-					<div className="item">
-						<div className="card">
-							<MDBCard
+
+					<div className="col-lg-4 col-md-6 mb-4">
+						<div className="card" style={{ width: "22rem", height: "32rem" }}>
+							<img
 								style={{
 									width: "22rem",
-									height: "32rem",
+									height: "12rem",
+									objectFit: "cover",
 								}}
+								src={sbTelecomWebstore}
+								alt="S.B. Telecom WebStore"
+							/>
+							<div
+								className="card-body"
+								style={{ textAlign: "center", borderColor: "#1f2235" }}
 							>
-								<MDBCardImage
-									src={sbTelecomWebstore}
-									position="top"
-									alt="..."
-								/>
-								<MDBCardBody
-									style={{
-										textAlign: "center",
-										borderColor: "#1f2235",
-									}}
+								<h5
+									className="card-title"
+									style={{ fontWeight: "bold", fontSize: "25px" }}
 								>
-									<MDBCardTitle
-										style={{
-											fontWeight: "bold",
-											fontSize: "25px",
-											textAlign: "center",
-										}}
-									>
-										{" "}
-										S.B. Telecom WebStore{" "}
-									</MDBCardTitle>{" "}
-									<MDBCardText>
-										An eCommerce application with admin dashboard for sell and
-										manage products and users using ReactJs, Mongo DB, Express
-										Js, Node Js, Redux, StripeJs, GraphJs.{" "}
-									</MDBCardText>{" "}
-									<MDBBtn
-										style={{
-											background: "#ff5823",
-										}}
-										href="https://github.com/king-knight-harsh/sbtelecom-webstore/tree/main/iteration_3"
-									>
-										{" "}
-										View{" "}
-									</MDBBtn>{" "}
-								</MDBCardBody>{" "}
-							</MDBCard>{" "}
-						</div>{" "}
+									S.B. Telecom WebStore
+								</h5>
+								<p className="card-text">
+									An eCommerce application with admin dashboard for selling and
+									managing products and users using ReactJs, MongoDB, ExpressJs,
+									NodeJs, Redux, StripeJs, GraphJs.
+								</p>
+								<a
+									href="https://github.com/king-knight-harsh/sbtelecom-webstore/tree/main/iteration_3"
+									className="btn btn-primary"
+									style={{ background: "#ff5823" }}
+								>
+									View
+								</a>
+							</div>
+						</div>
 					</div>
-					<div className="item">
-						<div className="card">
-							<MDBCard
+
+					<div className="col-lg-4 col-md-6 mb-4">
+						<div className="card" style={{ width: "22rem", height: "32rem" }}>
+							<img
 								style={{
 									width: "22rem",
-									height: "32rem",
+									height: "12rem",
+									objectFit: "cover",
 								}}
+								src={gameOfFocus}
+								alt="The Game of Focus"
+							/>
+							<div
+								className="card-body"
+								style={{ textAlign: "center", borderColor: "#1f2235" }}
 							>
-								<MDBCardImage src={gameOfFocus} position="top" alt="..." />
-								<MDBCardBody
-									style={{
-										textAlign: "center",
-										borderColor: "#1f2235",
-									}}
+								<h5
+									className="card-title"
+									style={{ fontWeight: "bold", fontSize: "25px" }}
 								>
-									<MDBCardTitle
-										style={{
-											fontWeight: "bold",
-											fontSize: "25px",
-											textAlign: "center",
-										}}
-									>
-										{" "}
-										The Game of Focus{" "}
-									</MDBCardTitle>{" "}
-									<MDBCardText>
-										Four player board game developed using java swing,
-										serialization module build using Java.{" "}
-									</MDBCardText>{" "}
-									<MDBBtn
-										style={{
-											background: "#ff5823",
-										}}
-										href="https://github.com/king-knight-harsh/comp2005-fall21-group4/tree/main/Group%204%20Iteration%204"
-									>
-										{" "}
-										View{" "}
-									</MDBBtn>{" "}
-								</MDBCardBody>{" "}
-							</MDBCard>{" "}
-						</div>{" "}
+									The Game of Focus
+								</h5>
+								<p className="card-text">
+									Four-player board game developed using Java Swing,
+									serialization module built using Java.
+								</p>
+								<a
+									href="https://github.com/king-knight-harsh/comp2005-fall21-group4/tree/main/Group%204%20Iteration%204"
+									className="btn btn-primary"
+									style={{ background: "#ff5823" }}
+								>
+									View
+								</a>
+							</div>
+						</div>
 					</div>
-				</OwlCarousel>
+				</div>
 			</section>
 		</div>
 	);
